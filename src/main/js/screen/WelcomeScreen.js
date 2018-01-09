21d231d9b79c59f59820bfe0b25f34ebc9825d5f
Ronald Ray <fritz.ray@eduworks.com>
2018-01-08 17:10:08 -0800

    Improved messaging.

--- src/main/js/screen/WelcomeScreen.js
+++ src/main/js/screen/WelcomeScreen.js
@@ -7,14 +7,22 @@ WelcomeScreen = (function(WelcomeScreen){
 
 		$("#frameworkManagementBtn").click(function(){
             ScreenManager.changeScreen(new CassEditorScreen());
+			if (!AppController.loginController.getLoggedIn())
+				alert("As you are not logged in, you will be restricted to viewing frameworks only.");
         });
 
+		if (!AppController.loginController.getLoggedIn())
+			$("#competencyAlignmentBtn").addClass("disabled");
+
 		$("#competencyAlignmentBtn").click(function(){
-            ScreenManager.changeScreen(new AlignmentEditorScreen());
+			if (!AppController.loginController.getLoggedIn())
+				alert("You should log in before attempting to align competencies.");
+			else
+            	ScreenManager.changeScreen(new AlignmentEditorScreen());
         });
 
 		$("#learnerProfilesBtn").click(function(){
-            ScreenManager.changeScreen(new AssertionSearchScreen());
+			alert("Not yet implemented.");
 		});
 	};
 
